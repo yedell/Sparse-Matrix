440c9da52c4dee6d408f84f931f4713d3d4e4941
yedell <yedell@mail.sfsu.edu>
2017-02-15 10:06:16 -0800

    Update Sparse.java

--- Sparse.java
+++ Sparse.java
@@ -22,64 +22,64 @@ public class Sparse {
 		
 		// Variables
 		Scanner input = new Scanner(new File(args[0]));
-        PrintWriter output = new PrintWriter(new File(args[1]));
-        int size = Integer.parseInt(input.next()), aNNZ = Integer.parseInt(input.next()), bNNZ = Integer.parseInt(input.next());
-        Matrix A = new Matrix(size);
-        Matrix B = new Matrix(size);
-        int k = 0;
-        
-        input.nextLine();
-        
-        while(k < aNNZ) {
-        	int i = Integer.parseInt(input.next()), j = Integer.parseInt(input.next());
-            double value = Double.parseDouble(input.next());
-            A.changeEntry(i, j, value);
-            input.nextLine();
-            k++;
-        }
-        
-        input.nextLine();
-        k = 0;
-        
-        while(k < bNNZ) {
-        	int i = Integer.parseInt(input.next()), j = Integer.parseInt(input.next());
-            double value = Double.parseDouble(input.next());
-            B.changeEntry(i, j, value);
-            input.nextLine();
-            k++;
-        }
-		
+		PrintWriter output = new PrintWriter(new File(args[1]));
+		int size = Integer.parseInt(input.next()), aNNZ = Integer.parseInt(input.next()), bNNZ = Integer.parseInt(input.next());
+		Matrix A = new Matrix(size);
+		Matrix B = new Matrix(size);
+		int k = 0;
+
+		input.nextLine();
+
+		while(k < aNNZ) {
+			int i = Integer.parseInt(input.next()), j = Integer.parseInt(input.next());
+		    double value = Double.parseDouble(input.next());
+		    A.changeEntry(i, j, value);
+		    input.nextLine();
+		    k++;
+		}
+
+		input.nextLine();
+		k = 0;
+
+		while(k < bNNZ) {
+			int i = Integer.parseInt(input.next()), j = Integer.parseInt(input.next());
+		    double value = Double.parseDouble(input.next());
+		    B.changeEntry(i, j, value);
+		    input.nextLine();
+		    k++;
+		}
+
 		output.println("A has " + A.getNNZ() + " non-zero entries:");
 		output.println(A);
-		
+
 		output.println("B has " + B.getNNZ() + " non-zero entries:");
 		output.println(B);
 		
 		output.println("(1.5)*A =");
 		output.println(A.scalarMult(1.5));
-		
+
 		output.println("A+B =");
 		output.println(A.add(B));
-		
+
 		output.println("A+A =");
 		output.println(A.add(A));
-		
+
 		output.println("B-A =");
 		output.println(B.sub(A));
-		
+
 		output.println("A-A =");
 		output.println(A.sub(A));
-		
+
 		output.println("Transpose(A) =");
 		output.println(A.transpose());
-		
+
 		output.println("A*B =");
 		output.println(A.mult(B));
-		
+
 		output.println("B*B =");
 		output.println(B.mult(B));
-		
+
 		input.close();
 		output.close();
-	}
+	}	
 }
