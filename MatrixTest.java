58c6b538aeea3574e8fc41cf6955dd5dfff0ca74
yedell <yedell@mail.sfsu.edu>
2017-02-15 10:07:59 -0800

    Update MatrixTest.java

--- MatrixTest.java
+++ MatrixTest.java
@@ -47,17 +47,17 @@ public class MatrixTest {
 		B.makeZero();
 		
 		A.changeEntry(1,1,1); B.changeEntry(1,1,1);
-	    A.changeEntry(1,2,2); B.changeEntry(1,2,0);
-	    A.changeEntry(1,3,3); B.changeEntry(1,3,-1);
-	    A.changeEntry(2,1,4); B.changeEntry(2,1,0);
-	    A.changeEntry(2,2,5); B.changeEntry(2,2,-1);
-	    A.changeEntry(2,3,6); B.changeEntry(2,3,0);
-	    A.changeEntry(3,1,7); B.changeEntry(3,1,-2);
-	    A.changeEntry(3,2,8); B.changeEntry(3,2,-3);
-	    A.changeEntry(3,3,9); B.changeEntry(3,3,-4);
+	    	A.changeEntry(1,2,2); B.changeEntry(1,2,0);
+	    	A.changeEntry(1,3,3); B.changeEntry(1,3,-1);
+	    	A.changeEntry(2,1,4); B.changeEntry(2,1,0);
+	    	A.changeEntry(2,2,5); B.changeEntry(2,2,-1);
+	    	A.changeEntry(2,3,6); B.changeEntry(2,3,0);
+	    	A.changeEntry(3,1,7); B.changeEntry(3,1,-2);
+	    	A.changeEntry(3,2,8); B.changeEntry(3,2,-3);
+	    	A.changeEntry(3,3,9); B.changeEntry(3,3,-4);
 	    
-	    System.out.println("Matrix A non-zero entries: " + A.getNNZ());
-	    System.out.println("Matrix A size: " + A.getSize());
+	    	System.out.println("Matrix A non-zero entries: " + A.getNNZ());
+	    	System.out.println("Matrix A size: " + A.getSize());
 		System.out.println("Matrix A created from column-ordered changeEntry:");
 		System.out.println(A);
 		
